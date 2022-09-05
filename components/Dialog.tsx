be9cb80dd515ee9c8d3ca8edbdf04fdc3d44cd39
pixeledcode <ashoaib540918@gmail.com>
2022-09-05 12:23:50 +0530

    update dialog to remove styles

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -23,17 +23,8 @@ const StyledOverlay = styled(DialogPrimitive.Overlay, {
 });
 
 const StyledContent = styled(DialogPrimitive.Content, {
-  backgroundColor: 'white',
-  borderRadius: 6,
-  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
   position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '90vw',
-  maxWidth: '450px',
-  maxHeight: '85vh',
-  padding: 25,
+
   zIndex: '$max',
   '@media (prefers-reduced-motion: no-preference)': {
     animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
@@ -56,24 +47,10 @@ function Content({ children, ...props }: DialogContentProps) {
   );
 }
 
-const StyledTitle = styled(DialogPrimitive.Title, {
-  margin: 0,
-  fontWeight: 500,
-  color: '$textLightHigh',
-  fontSize: 17,
-});
-
-const StyledDescription = styled(DialogPrimitive.Description, {
-  margin: '10px 0 20px',
-  color: '$textLightMedium',
-  fontSize: 15,
-  lineHeight: 1.5,
-});
-
 // Exports
 export const Dialog = DialogPrimitive.Root;
 export const DialogTrigger = DialogPrimitive.Trigger;
 export const DialogContent = Content;
-export const DialogTitle = StyledTitle;
-export const DialogDescription = StyledDescription;
+export const DialogTitle = DialogPrimitive.Title;
+export const DialogDescription = DialogPrimitive.Description;
 export const DialogClose = DialogPrimitive.Close;
